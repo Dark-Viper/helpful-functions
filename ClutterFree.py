13e147b335c5f931c851bc26ff0bf8de16277a6f
Dark-Viper <45880597+Dark-Viper@users.noreply.github.com>
2018-12-14 17:34:31 +0100

    Add files via upload
    This python code is designed to help you clear that clutter off your screen. It is a witty function, in sum. Helped me a great deal along my journey. I hope this helps you too :D

---
+++ ClutterFree.py
@@ -0,0 +1,7 @@
+# Python: Simple Function to empty out your screen. Free software by Tn aka Dark Viper
+space = ' '
+zipcode = 99999
+def clr ():
+	print (space * zipcode)
+	print ('Cleared. Utility Software by the Dark Viper')
+	cls ()
