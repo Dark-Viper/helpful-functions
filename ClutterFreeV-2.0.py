8e2eb1e0957a66dda3b34f4c175624b2735819cf
Dark Viper <45880597+Dark-Viper@users.noreply.github.com>
2018-12-15 11:37:34 +0100

    Add files via upload
    This python code is designed to help you clear that clutter off your screen. It is a witty function, in sum. Helped me a great deal along my journey. I hope this helps you too :D 
    Update: 
    *switched variables to allow compatibility with long scripts with large databases

---
+++ ClutterFreeV-2.0.py
@@ -0,0 +1,7 @@
+# Python: Simple Function to empty out your screen. Free software by Tn aka Dark Viper
+s1342_48xsb24_012ab = ' '
+z2431_42bsx84_ba210 = 99999
+def clr ():
+	print (s1342_48xsb24_012ab * z2431_42bsx84_ba210)
+	print ('Cleared. Utility Software by the Dark Viper')
+	cls ()
